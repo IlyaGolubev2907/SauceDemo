5b309edc44c45055c2847c42b8fcbca1089648a9
IlyaGolubev2907 <Ilya.Golubev2907@gmail.com>
2025-07-28 18:43:21 +0300

    fix BaseTest

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -4,6 +4,9 @@ import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class ProductsPage extends LoadablePage {
     private final By TITLE = By.xpath("//span[text()='Products']");
